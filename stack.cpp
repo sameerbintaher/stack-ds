eb21b7aca51b23c05a592177cedf21fa73ff8de1
Sameer <macbook@SameerBook.local>
2022-08-16 23:15:08 +0600

    first commit

---
+++ stack.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+     int value;
+     Node *next;
+     Node *prev;
+
+     Node(int val)
+     {
+          value = val;
+          next = NULL;
+          prev = NULL;
+     }
+};
+
+class Stack
+{
+     Node *head;
+     Node *top;
+
+     // PUSH
+     void push(int val)
+     {
+          Node *newNode = new Node(val);
+          if (head == NULL)
+          {
+               head = top = newNode;
+               return;
+          }
+          top->next = newNode;
+          newNode->prev = top;
+          top = newNode;
+     }
+};
+
+int main()
+{
+}
